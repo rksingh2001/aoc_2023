807c948e53874cb18de079368d6352e9a53370ac
raunak <rksj2001@gmail.com>
2023-12-09 16:08:28 +0530

    improved code little bit

--- 1.ex
+++ 1.ex
@@ -1,17 +1,16 @@
-defmodule FileReader do
-  def print_file(file_path) do
-    file_stream = File.stream!(file_path)
-    sol = Enum.reduce(file_stream, 0, fn line, acc ->
-      line = String.to_charlist(line)
-      nums = Enum.filter(line, fn char -> char in ?0..?9 end)
-      first_num = hd(nums)
-      nums = Enum.reverse(nums)
-      last_num = hd(nums)
-      num = List.to_integer([first_num, last_num])
-      num + acc
-    end)
-    IO.puts(sol)
-  end
-end
+file_path = "1.txt"
 
-FileReader.print_file("1.txt")
+file_stream = File.stream!(file_path)
+
+sol =
+  Enum.reduce(file_stream, 0, fn line, acc ->
+    line = String.to_charlist(line)
+    nums = Enum.filter(line, fn char -> char in ?0..?9 end)
+    first_num = hd(nums)
+    nums = Enum.reverse(nums)
+    last_num = hd(nums)
+    num = List.to_integer([first_num, last_num])
+    num + acc
+  end)
+
+IO.puts(sol)
